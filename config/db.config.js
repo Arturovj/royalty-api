5d5071f94a08ada70025fad7488fa869b1118146
Arturo Vivar <arthas1237@hotmail.com>
2022-05-06 19:57:24 +0200

    try3

--- config/db.config.js
+++ config/db.config.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 const DB_NAME = 'royalty';
 const URI = process.env.MONGODB_URI || '';
-const DB_URI = `${URI}/${DB_NAME}`;
+// const DB_URI = `${URI}/${DB_NAME}`;
 
 mongoose
-  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.info(`Successfully connected to the database ${DB_URI}`))
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info(`Successfully connected to the database ${URI}`))
   .catch((error) => {
-    console.error(`An error ocurred trying to connect to de database ${DB_URI}`, error);
+    console.error(`An error ocurred trying to connect to de database ${URI}`, error);
     process.exit(0);
   });
 
